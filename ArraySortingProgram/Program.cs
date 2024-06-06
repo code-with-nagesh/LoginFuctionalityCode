a2cbec1aaf84a12121c9c4e231c97af74effc554
nagesh <kadamnagesh4294@gmail.com>
2024-06-06 11:44:47 +0530

    this is second test

---
+++ ArraySortingProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] input = { 6, 8, 7, 2, 5, 1, 3, 4 };
+
+        Array.Sort(input);
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            Console.WriteLine(input[i]);
+        }
+    }
+
+}
---
+++ DifferanceBetweenAbstractionAndInterface/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+// Abstract class
+abstract class Animal
+{
+    public abstract void MakeSound();
+    public void Sleep()
+    {
+        Console.WriteLine("Sleeping...");
+    }
+}
+
+// Interface
+interface IFlyable
+{
+    void Fly();
+}
+
+// Derived class implementing both an abstract class and an interface
+class Bird : Animal, IFlyable
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Chirp");
+    }
+
+    public void Fly()
+    {
+        Console.WriteLine("Flying...");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Bird myBird = new Bird();
+        myBird.MakeSound(); // Output: Chirp
+        myBird.Sleep();     // Output: Sleeping...
+        myBird.Fly();       // Output: Flying...
+    }
+}
